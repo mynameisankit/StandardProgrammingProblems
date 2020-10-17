9c186a4a7dcfae25c93a2a46304cf36e9977c1de
Ankit Kumar <ankitamuniverse@gmail.com>
2020-10-17 21:23:23 +0530

    Update top_view_of_binary_tree.cpp

--- Binary Tree/top_view_of_binary_tree.cpp
+++ Binary Tree/top_view_of_binary_tree.cpp
@@ -1,93 +1,10 @@
-// { Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node
-{
-    int data;
-    struct Node *left;
-    struct Node *right;
-
-    Node(int x){
-        data = x;
-        left = NULL;
-        right = NULL;
-    }
-};
-
-void topView(struct Node *root);
-
-
-int main()
-{
-    int t;cin>>t;
-    while(t--)
-    {
-        int n,i,k;
-        cin>>n;
-        i=n;
-
-        Node* root=NULL;
-        queue <Node*> q;
-        while(i>0)
-        {
-            int a,b;
-            char c;
-            cin>> a >> b >> c;
-            if(!root){
-                root = new Node(a);
-                q.push(root);
-            }
-            Node* pick = q.front();
-            q.pop();
-
-            if(c == 'L'){
-                pick->left = new Node(b);
-                q.push( pick->left );
-            }
-            cin>> a >> b >> c;
-            if(c == 'R'){
-                pick->right = new Node(b);
-                q.push( pick->right );
-            }
-            i-=2;
-        }
-//        inorder(root);
-//        cout<<endl;
-        topView(root);
-        cout << endl;
-    }
-    return 0;
-}
-
-
-
-
-// } Driver Code Ends
-
-
-//Structure of binary tree
-/*struct Node
-struct Node
-{
-    int data;
-    struct Node* left;
-    struct Node* right;
-    
-    Node(int x){
-        data = x;
-        left = right = NULL;
-    }
-};*/
-// function should print the topView of the binary tree
-
-void tv(Node *root, map<int, int>& hd_and_value, map<int, int>& hd_and_depth , int depth, int hd) {
+void topView_helper(Node *root, map<int, int>& hd_and_value, map<int, int>& hd_and_depth , int depth, int hd) {
     if(root == NULL) {
         return;
     }
     else {
         //Left Call
-        tv(root->left, hd_and_value, hd_and_depth, depth + 1, hd - 1);
+        topView_helper(root->left, hd_and_value, hd_and_depth, depth + 1, hd - 1);
         
         bool flag = false;
 
@@ -117,7 +34,7 @@ void tv(Node *root, map<int, int>& hd_and_value, map<int, int>& hd_and_depth , i
         }
         
         //Right Call
-        tv(root->right, hd_and_value, hd_and_depth, depth + 1, hd + 1);
+        topView_helper(root->right, hd_and_value, hd_and_depth, depth + 1, hd + 1);
         
         return;
     }
@@ -128,7 +45,7 @@ void topView(struct Node *root) {
    map<int, int> hd_and_value; 
    map<int, int> hd_and_depth; 
    
-   tv(root, hd_and_value, hd_and_depth, 0, 0);
+   topView_helper(root, hd_and_value, hd_and_depth, 0, 0);
    
    for(map<int, int>::iterator it = hd_and_value.begin(); it != hd_and_value.end(); it++) {
        cout << it->second << " ";
